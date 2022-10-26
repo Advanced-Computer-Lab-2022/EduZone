9542f031172fdfe9c02a2f4ff8d2f29c662e7b10
Mohamed Medhat <mmedhat1910@gmail.com>
2022-10-26 11:25:39 +0200

    courses with count

--- backend/src/controllers/coursesController.ts
+++ backend/src/controllers/coursesController.ts
@@ -5,67 +5,160 @@ export const getAllCourses = async (
   page?: string,
   limit?: string
 ) => {
-  const { title, subject, instructor, price, rating } = filters;
-  const courses = CourseModel.find(filters).populate('instructor', [
-    'name',
-    'username',
-    '_id',
-    'email',
-  ]);
-
-  if (title) {
-    courses.find({ title: { $regex: title, $options: 'i' } });
-  }
-
-  if (subject) {
-    courses.find({ subject: { $regex: subject, $options: 'i' } });
-  }
-
+  const { title, subject, instructor, price, rating, maxPrice, minPrice } =
+    filters;
+  let instructorId: any = '';
   if (instructor) {
     //get instructor id by name
-    const instructorId = await UserModel.findOne({
+    instructorId = await UserModel.findOne({
       name: { $regex: instructor, $options: 'i' },
     }).select('_id');
-
-    courses.find({ instructor: instructorId });
-  }
-  if (price) {
-    if (price.includes('-')) {
-      const [min, max] = price.split('-');
-      courses.find({ price: { $gte: min, $lte: max } });
-    } else if (price.includes('>')) {
-      const min = price.split('>')[1];
-      courses.find({ price: { $gte: min } });
-    } else if (price.includes('<')) {
-      const max = price.split('<')[1];
-      courses.find({ price: { $lte: max } });
-    } else {
-      courses.find({ price: price });
-    }
   }
 
-  if (rating) {
-    if (rating.includes('>')) {
-      const min = rating.split('>')[1];
-      courses.find({ rating: { $gte: min } });
-    } else if (rating.includes('<')) {
-      const max = rating.split('<')[1];
-      courses.find({ rating: { $lte: max } });
-    } else {
-      courses.find({ rating: rating });
-    }
+  const query = {
+    ...(title && { title: { $regex: title, $options: 'i' } }),
+    ...(subject && { subject: { $regex: subject, $options: 'i' } }),
+    ...(instructor && { instructor: instructorId._id }),
+    ...(price && { price: price }),
+    ...(rating && { rating: { $gte: rating } }),
+    ...(maxPrice && { price: { $lte: maxPrice } }),
+    ...(minPrice && { price: { $gte: minPrice } }),
+  };
+  const courses = CourseModel.aggregate([
+    {
+      $match: query,
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'instructor',
+        foreignField: '_id',
+        as: 'instructor',
+      },
+    },
+    {
+      $unwind: {
+        path: '$instructor',
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $project: {
+        title: 1,
+        subject: 1,
+        instructor: {
+          _id: 1,
+          username: 1,
+          name: 1,
+          avatar: 1,
+        },
+        price: 1,
+        rating: 1,
+        createdAt: 1,
+      },
+    },
+  ]);
+
+  if (page && limit) {
+    const currentPage = parseInt(page);
+    const pageSize = parseInt(limit);
+    const skip = (currentPage - 1) * pageSize;
+    const total = await CourseModel.countDocuments(query);
+    const totalPages = Math.ceil(total / pageSize);
+    const results = await courses.skip(skip).limit(pageSize).exec();
+    return {
+      courses: results,
+      pagination: {
+        currentPage,
+        totalPages,
+        pageSize,
+        total,
+      },
+    };
   }
 
-  if (limit && page) {
-    const skip = (parseInt(page) - 1) * parseInt(limit);
-    courses.skip(skip).limit(parseInt(limit));
-  }
-
-  // get courses count
-  // const count = await (await courses).length;
-  // return { data: courses, count };
-  return courses;
+  return { courses: await courses.skip(0).exec() };
 };
+
+const getPriceQuery = (price: string) => {
+  if (price.includes('-')) {
+    const [min, max] = price.split('-');
+    return { $gte: min, $lte: max };
+  } else if (price.includes('>')) {
+    const min = price.split('>')[1];
+    console.log(
+      '🚀 ~ file: coursesController.ts ~ line 94 ~ getPriceQuery ~ min',
+      min
+    );
+    return { $gte: min };
+  } else if (price.includes('<')) {
+    const max = price.split('<')[1];
+    return { $lte: max };
+  } else {
+    return price;
+  }
+};
+
+// const courses = CourseModel.find(filters).populate('instructor', [
+//   'name',
+//   'username',
+//   '_id',
+//   'email',
+// ]);
+
+// if (title) {
+//   courses.find({ title: { $regex: title, $options: 'i' } });
+// }
+
+// if (subject) {
+//   courses.find({ subject: { $regex: subject, $options: 'i' } });
+// }
+
+// if (instructor) {
+//   //get instructor id by name
+//   const instructorId = await UserModel.findOne({
+//     name: { $regex: instructor, $options: 'i' },
+//   }).select('_id');
+
+//   courses.find({ instructor: instructorId });
+// }
+// if (price) {
+//   if (price.includes('-')) {
+//     const [min, max] = price.split('-');
+//     courses.find({ price: { $gte: min, $lte: max } });
+//   } else if (price.includes('>')) {
+//     const min = price.split('>')[1];
+//     courses.find({ price: { $gte: min } });
+//   } else if (price.includes('<')) {
+//     const max = price.split('<')[1];
+//     courses.find({ price: { $lte: max } });
+//   } else {
+//     courses.find({ price: price });
+//   }
+// }
+
+// if (rating) {
+//   if (rating.includes('>')) {
+//     const min = rating.split('>')[1];
+//     courses.find({ rating: { $gte: min } });
+//   } else if (rating.includes('<')) {
+//     const max = rating.split('<')[1];
+//     courses.find({ rating: { $lte: max } });
+//   } else {
+//     courses.find({ rating: rating });
+//   }
+// }
+
+// if (limit && page) {
+//   const skip = (parseInt(page) - 1) * parseInt(limit);
+//   courses.skip(skip).limit(parseInt(limit));
+// }
+
+// get courses count
+// const count = await (await courses).length;
+// return { data: courses, count };
+// return courses;
+// };
 
 //Add a new Course
 export const addCourse = (data: typeof CourseModel) => {
