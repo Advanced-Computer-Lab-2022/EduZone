95da59a0fef57662b5d749a64dd36629a2d3a8f1
Mohamed Medhat <mmedhat1910@gmail.com>
2022-10-29 22:34:44 +0200

    feat/search bar working

--- frontend/src/pages/courses/courses.tsx
+++ frontend/src/pages/courses/courses.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import { AxiosResponse } from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import Layout from '../../components/layout/Layout';
+import { axios } from '../../utils';
 
 const Courses = () => {
+  const [courses, setCourses] = useState([] as any[]);
+  const [pagination, setPagination] = useState({} as any);
+  const [searchParams] = useSearchParams();
+
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const getCourses = async () => {
+    const query = searchParams.get('query');
+    try {
+      const res: AxiosResponse<any, any> = await axios({
+        url: '/courses',
+        method: 'GET',
+        params: {
+          query,
+          page,
+          limit,
+        },
+      });
+      console.log(res.data);
+      setCourses(res.data?.courses);
+      setPagination(res.data?.pagination);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getCourses();
+  }, [searchParams]);
   return (
     <Layout>
-      <div>Courses</div>
+      <div>
+        Courses | Total: {pagination?.total} | Page: {pagination?.currentPage}|
+        Search Results for "{searchParams?.get('query')}"
+      </div>
     </Layout>
   );
 };
