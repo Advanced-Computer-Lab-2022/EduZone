24513f9dbf8533c62b525d8a029e0a239eb5014c
Mohamed Medhat <mmedhat1910@gmail.com>
2022-12-15 00:44:48 +0200

    view grades of each exercise

--- frontend/src/pages/courses/course/LearningPage.tsx
+++ frontend/src/pages/courses/course/LearningPage.tsx
@@ -162,30 +162,48 @@ const LearningPage = () => {
                 ) && (
                   <div
                     className={`flex items-center justify-between  border rounded-md  p-4 ${
-                      score >= 50
+                      score >= 75
                         ? 'border-green-600 text-green-700 bg-green-500/20'
+                        : score >= 50
+                        ? 'border-yellow-600 text-yellow-700 bg-yellow-500/20'
                         : 'border-red-600 text-red-700 bg-red-500/20'
                     }  w-full mb-4`}
                   >
                     <p>
-                      You have already completed this exercise with score:{' '}
-                      <span className="font-bold">{score}%</span>
+                      You have submitted your answer successfully
                       <p className="text-sm">
-                        You can review your answers below
+                        You can review your answers below.{' '}
+                        {score < 50 && (
+                          <span className="underline hover:font-semibold cursor-pointer">
+                            Retake the exercise
+                          </span>
+                        )}
                       </p>
                     </p>
-                    <p className={'text-xl font-semibold'}>
-                      {
-                        // enrolled?.exercises.find(
-                        //   (e: any) =>
-                        //     e.exerciseId ===
-                        //     courseItems[currentCourseItem - 1]?.data?._id.toString()
-                        // )?.score >= 50
-                        score >= 50 ? 'PASSED' : 'FAILED'
-                      }
-                    </p>
+                    <div className="flex flex-col">
+                      <p>Your Score:</p>
+                      <p className={'text-xl font-semibold'}>
+                        <span className="font-bold">{score}% - </span>
+                        {score >= 50 ? 'PASSED' : 'FAILED'}
+                      </p>
+                    </div>
                   </div>
                 )}
+                {score > -1 ? (
+                  <div className="flex w-full justify-end gap-2 text-sm mb-3">
+                    {score < 100 && (
+                      <button className="hover:underline text-gray-600 pr-2 border-r-2 border-gray-400">
+                        View Correct Answers
+                      </button>
+                    )}
+                    {score < 50 && (
+                      <button className="hover:underline text-gray-600">
+                        {' '}
+                        Retake the exercise
+                      </button>
+                    )}
+                  </div>
+                ) : null}
                 <ExerciseView
                   exercise={courseItems[currentCourseItem - 1]?.data}
                   subtitleId={courseItems[currentCourseItem - 2]?.data?._id}
