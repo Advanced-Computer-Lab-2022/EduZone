29ad11d4775cc1292aae54d88716e721e79cbc71
Mohamed Medhat <mmedhat1910@gmail.com>
2022-12-15 00:57:20 +0200

    forget password page

--- frontend/src/pages/ForgetPassword.tsx
+++ frontend/src/pages/ForgetPassword.tsx
@@ -33,40 +33,47 @@ const ForgetPassword = () => {
       });
 
       if (res.status === 200) {
-        console.log(res);
+        setLoading(false);
+        navigate('/login');
       }
     } catch (err) {
       console.log(err);
     }
-    console.log('Forget Password');
   };
   return (
-    <div className="w-3/5  flex justify-center items-center">
-      <form
-        className="bg-white w-1/2 h-2/3 rounded-lg shadow-lg p-10 py-16 flex flex-col justify-evenly"
-        onSubmit={onSubmit}
-      >
-        <div className="">
-          <p className="text-gray-600">Please enter your Email</p>
-        </div>
-        <div className="space-y-4">
-          <InputField
-            placeholder="email@email.com"
-            icon={
-              <MdEmail size={25} className=" text-gray-500 absolute ml-1" />
-            }
-            name="email"
-          />
-          <input
-            value={'Submit'}
-            type="submit"
-            className="bg-primary text-white w-full py-3 rounded-sm flex items-center justify-center"
-          />
-          {/* <CircularLoadingIndicator loading={loading} /> */}
-          {/* Login */}
-          {/* </button> */}
-        </div>
-      </form>
+    <div className="h-screen from-secondary-dark to-primary bg-gradient-to-br flex items-center justify-center">
+      <div className="w-3/5  flex justify-center items-center">
+        <form
+          className="bg-white w-1/2 h-2/3 rounded-lg shadow-lg p-10 py-16 flex flex-col justify-evenly"
+          onSubmit={onSubmit}
+        >
+          <div className="space-y-6">
+            <div>
+              <p className="text-3xl font-medium text-center">
+                Forget Password
+              </p>
+              <p className="text-sm text-center text-gray-400 ">
+                We are going to send you an email with the reset link
+              </p>
+            </div>
+            <div className="">
+              <p className="text-gray-600">Please enter your Email</p>
+              <InputField
+                placeholder="email@email.com"
+                icon={
+                  <MdEmail size={25} className=" text-gray-500 absolute ml-1" />
+                }
+                name="email"
+              />
+            </div>
+            <input
+              value={'Submit'}
+              type="submit"
+              className="bg-primary text-white w-full py-3 rounded-sm flex items-center justify-center cursor-pointer"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
